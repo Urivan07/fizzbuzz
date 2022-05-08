c718c7e83b7a0458dca65add2ba786c8d8b31ca3
Urivan <urivan07@gmail.com>
2022-05-08 17:49:25 -0500

    Se agrega express server

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -2,11 +2,11 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Test para ExplorerController", ()=>{
     test("obteniendo explorers con mision node", () =>{
-        const ExplorersByMission = ExplorerController.getExplorersByMission("node")
-        expect(ExplorersByMission.length).toBe(10)
-    })
+        const ExplorersByMission = ExplorerController.getExplorersByMission("node");
+        expect(ExplorersByMission.length).toBe(10);
+    });
     test("obteniendo explorers con mision java", () =>{
-        const ExplorersByMission = ExplorerController.getExplorersByMission("java")
-        expect(ExplorersByMission.length).toBe(5)
-    })
-})
+        const ExplorersByMission = ExplorerController.getExplorersByMission("java");
+        expect(ExplorersByMission.length).toBe(5);
+    });
+});
