f4b34e859a99d2ee9df216263ee93e61d95f3359
Urivan <urivan07@gmail.com>
2022-05-08 15:55:20 -0500

    Agregando funcionalidad de metodo getAmountOfExplorersByMission de ExplorerService y prubea unitaria

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,11 +1,23 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
+const fs = require("fs");
+const rawdata = fs.readFileSync("explorers.json"); // Read json file
+const ExplorerJson = JSON.parse(rawdata);
+
+
 describe("Test para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una mission", () => {
         const explorers = [{ mission: "node"}];
         const explorerInNode = ExplorerService.filterByMission(explorers, "node");
-
+        
         expect(explorerInNode.length).toBe(1);
-
     });
+
+    test("Amount of Explorers by mission", () => {
+        const explorers = ExplorerJson
+        const explorerInJava = ExplorerService.getAmountOfExplorersByMission(explorers, "java")
+        
+        expect(explorerInJava).toBe(5)
+
+    })
 });
