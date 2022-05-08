2951ea4d8b0def9e89b7fb1a7105627db0aad1d8
Urivan <urivan07@gmail.com>
2022-05-08 17:15:22 -0500

    Agregando capturas de test realizado para explorerService y se agrega archivo app el cual fue refactorizado

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,13 +3,10 @@ const ExplorerService = require("./../../lib/services/ExplorerService");
 const Reader = require("./../../lib/utils/Reader");
 const explorers = Reader.readJsonFile("./explorers.json");
 
-
-
 describe("Test para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una mission", () => {
-        const explorers = [{ mission: "node"}];
         const explorerInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorerInNode.length).toBe(1);
+        expect(explorerInNode.length).toBe(10);
     });
     test("Amount of Explorers by mission", () => {
         const explorerInJava = ExplorerService.getAmountOfExplorersByMission(explorers, "java");
